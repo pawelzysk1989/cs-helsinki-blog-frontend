49d5e79ac9926e2ae577a0c23f397a4db100db9d
pawelzysk <pawelzysk1989@gmail.com>
2022-04-25 21:08:16 +0200

    package.json tidy up

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001/',
-        changeOrigin: true,
-      },
-    },
-  },
 });
